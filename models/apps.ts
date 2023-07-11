bd01439c69b5b88cb76a758465c871f9efa14c33
koudyt <digitalniweb@gmail.com>
2023-07-11 08:44:48 +0200

    apps - namespace removed

--- models/apps.ts
+++ models/apps.ts
@@ -6,49 +6,49 @@ import {
 	InferCreationAttributes,
 	ForeignKey,
 } from "sequelize";
-export declare namespace apps {
-	/**
-	 * current app's AdminMenus
-	 */
-	export interface AppAdminMenu
-		extends Model<
-			InferAttributes<AppAdminMenu>,
-			InferCreationAttributes<AppAdminMenu>
-		> {
-		id: CreationOptional<number>;
-		adminMenuId: number;
-	}
-	/**
-	 * current app's possible Languages
-	 */
-	export interface AppLanguage
-		extends Model<
-			InferAttributes<AppLanguage>,
-			InferCreationAttributes<AppLanguage>
-		> {
-		id: CreationOptional<number>;
-		languageId: number;
-	}
-	/**
-	 * current app's possible Modules
-	 */
-	export interface AppModule
-		extends Model<
-			InferAttributes<AppModule>,
-			InferCreationAttributes<AppModule>
-		> {
-		id: CreationOptional<number>;
-		moduleId: number;
-	}
-	/**
-	 * current app's possible Widget
-	 */
-	export interface AppWidget
-		extends Model<
-			InferAttributes<AppWidget>,
-			InferCreationAttributes<AppWidget>
-		> {
-		id: CreationOptional<number>;
-		widgetId: number;
-	}
+/* export declare namespace apps {	
+} */
+/**
+ * current app's AdminMenus
+ */
+export interface AppAdminMenu
+	extends Model<
+		InferAttributes<AppAdminMenu>,
+		InferCreationAttributes<AppAdminMenu>
+	> {
+	id: CreationOptional<number>;
+	adminMenuId: number;
+}
+/**
+ * current app's possible Languages
+ */
+export interface AppLanguage
+	extends Model<
+		InferAttributes<AppLanguage>,
+		InferCreationAttributes<AppLanguage>
+	> {
+	id: CreationOptional<number>;
+	languageId: number;
+}
+/**
+ * current app's possible Modules
+ */
+export interface AppModule
+	extends Model<
+		InferAttributes<AppModule>,
+		InferCreationAttributes<AppModule>
+	> {
+	id: CreationOptional<number>;
+	moduleId: number;
+}
+/**
+ * current app's possible Widget
+ */
+export interface AppWidget
+	extends Model<
+		InferAttributes<AppWidget>,
+		InferCreationAttributes<AppWidget>
+	> {
+	id: CreationOptional<number>;
+	widgetId: number;
 }
