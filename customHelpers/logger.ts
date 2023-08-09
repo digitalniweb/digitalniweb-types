a0c4d5a93422bab51db4ce4690e62d6b02fedbcc
koudyt <digitalniweb@gmail.com>
2023-08-09 20:19:21 +0200

    customErrorObject renamed to customLogObject

--- customHelpers/logger.ts
+++ customHelpers/logger.ts
@@ -1,12 +1,19 @@
+import { logTypes } from "../../digitalniweb-custom/variables/logs";
+export type logTypes = (typeof logTypes)[number];
+
 /**
- * @param {any|undefined} error JS error object
- * @param {number|string|undefined} code custom error code
- * @param {string|undefined} message custom error message
- * @param {any|undefined} data custom object with additional data which will be appended to response
+ * @property {any|undefined} error JS error object - usually have 'message', 'name', 'stack' properties
+ * @property {number|string|undefined} code custom error code
+ * @property {string|undefined} message custom error message
+ * @property {any|undefined} data custom object with additional data which will be appended to response
+ * @property {string|undefined} date UTC 0 time zone date
+ * @property {logTypes} type "error", "warning"... If there is `code` or `error.code` then type='http'
  */
-export type customErrorObject = {
+export type customLogObject = {
 	error?: any;
-	code?: number | string;
+	code?: number;
 	message?: string;
 	data?: any;
+	date?: string;
+	type?: logTypes;
 };
