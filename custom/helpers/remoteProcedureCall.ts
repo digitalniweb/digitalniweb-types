a043811d2ce6d6811ff9e860e64e60c27497b59f
koudyt <digitalniweb@gmail.com>
2023-11-26 14:28:37 +0100

    remoteProcedureCall type added

---
+++ custom/helpers/remoteProcedureCall.ts
@@ -0,0 +1,21 @@
+import { microservices } from "~/digitalniweb-types";
+import HTTPMethods from "~/digitalniweb-types/httpMethods";
+
+/**
+ * @property { [key: string]: any } `data` POST data
+ * @property { [key: string]: any } `params` GET parameters (query)
+ * @property  'single' | 'all' `scope` 'single' calls single service (main if `id` is not supplied). 'all' creates calls to all services and waits for first non-null result.
+ */
+export type msCallOptions = {
+	name: microservices;
+	req?: Request;
+	protocol?: "http" | "https";
+	id?: number;
+	path: string;
+	method?: HTTPMethods;
+	data?: { [key: string]: any };
+	params?: { [key: string]: any };
+	headers?: HeadersInit;
+	scope?: "single" | "all";
+	timeout?: number;
+};
