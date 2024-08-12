32122e17678cda98c08a1ebb50917b63416ab1af
koudyt <digitalniweb@gmail.com>
2024-08-12 21:40:14 +0200

    h3 event context verifiedUser added

--- custom.ts
+++ custom.ts
@@ -1,6 +1,12 @@
 import { customLogObject } from "./customHelpers/logger.js";
+import { userJWT } from "./users.js";
 declare module "express-serve-static-core" {
 	export interface NextFunction {
 		(err?: customLogObject): void;
+	}
+}
+declare module "h3" {
+	interface H3EventContext {
+		verifiedUser: userJWT;
 	}
 }
