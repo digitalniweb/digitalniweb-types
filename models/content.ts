6e7e951244c3693ddcc3d568e8fc5537df49b226
koudyt <digitalniweb@gmail.com>
2024-12-24 13:38:16 +0100

    content options string + websitesMsId nonattribute remove

--- models/content.ts
+++ models/content.ts
@@ -49,7 +49,7 @@ export interface WidgetContent
 	moduleRecordId: number;
 	name: string;
 	content: string;
-	options?: object;
+	options?: string; // stringified object
 	active?: boolean;
 	order: CreationOptional<number>;
 	createdAt?: CreationOptional<Date>;
