31d44129b3e368d48955c08340ebd07ae099dbba
koudyt <digitalniweb@gmail.com>
2025-08-31 17:46:32 +0200

    extra modules type delete

--- apps/communication/modules/index.ts
+++
@@ -1,7 +0,0 @@
-import type { InferAttributes } from "sequelize";
-import type { modulesWidgetsContent } from "../../../../digitalniweb-types/functionality/widgets";
-// !!! delete this file after changing this to ArticleWidget type returned in admin maybe?
-export type moduleResponse<T> = {
-	moduleInfo: T;
-	widgetContents: InferAttributes<modulesWidgetsContent>[];
-};
